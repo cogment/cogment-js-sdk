e6e493eaffa607efcca06007ff2d3b89f6cdd50b
Emma Coutu <emma@coutu.io>
2020-12-09 18:06:54 -0500

    [#4] Updates to cogment-app

--- __tests__/end-to-end/cogment-app/clients/web/main.js
+++ __tests__/end-to-end/cogment-app/clients/web/main.js
@@ -1,7 +1,5 @@
-
-
 import {Connection} from 'cogment';
-import cog_settings from './cog_settings';
+import {CogSettings} from 'cogment';
 import * as data_pb2 from './data_pb.js';
 
 const human_class = cog_settings.actor_classes.client;
@@ -12,37 +10,37 @@ let trial, observation, human_id;
 let endpoint = `http://${hostname}:8088`;
 
 async function start() {
+  const conn = new Connection(cog_settings, endpoint);
 
-    const conn = new Connection(cog_settings, endpoint);
-
-    if (!trial) {
-        try {
-            console.log("start trial");
-            trial = await conn.start_trial(cog_settings.actor_classes.client);
-            human_id = trial.id.charAt(trial.id.length - 1);
-        } catch (e) {
-            console.error(e);
-            return
-        }
+  if (!trial) {
+    try {
+      console.log('start trial');
+      trial = await conn.start_trial(cog_settings.actor_classes.client);
+      human_id = trial.id.charAt(trial.id.length - 1);
+    } catch (e) {
+      console.error(e);
+      return;
     }
+  }
 
-    console.log(`Client Trial now established, id: ${trial.id} + human id: ${human_id}`);
+  console.log(
+    `Client Trial now established, id: ${trial.id} + human id: ${human_id}`,
+  );
 
-	const action = new human_class.action_space();
+  const action = new human_class.action_space();
 
-	observation = await trial.do_action(action).catch(e => {
-		console.log(e)
-	});
-	observation = await trial.do_action(action).catch(e => {
-		console.log(e)
-	});
-	console.log('observation: ' + observation)
+  observation = await trial.do_action(action).catch((e) => {
+    console.log(e);
+  });
+  observation = await trial.do_action(action).catch((e) => {
+    console.log(e);
+  });
+  console.log('observation: ' + observation);
 
-	trial.end()
-
+  trial.end();
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("Dom loaded");
-    start();
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('Dom loaded');
+  start();
 });
