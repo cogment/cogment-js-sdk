78ab264e72d83509e3647249d4dbfa700cae5c83
Emma Coutu <emma@coutu.io>
2020-12-24 14:44:08 -0500

    feat(logger): implement DebugLogger using the `debug` package

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -14,10 +14,12 @@
  *  limitations under the License.
  *
  */
+import debug from 'debug';
+
 export type LoggerFunction = (...data: unknown[]) => void;
 
 export enum LogLevel {
-  trace,
+  trace = 1,
   debug,
   info,
   warn,
@@ -32,6 +34,74 @@ export interface Logger {
   info: LoggerFunction;
   trace: LoggerFunction;
   warn: LoggerFunction;
+}
+
+class DebugLogger implements Logger {
+  private readonly traceLogger: debug.Debugger;
+  private readonly debugLogger: debug.Debugger;
+  private readonly infoLogger: debug.Debugger;
+  private readonly warnLogger: debug.Debugger;
+  private readonly errorLogger: debug.Debugger;
+  private readonly fatalLogger: debug.Debugger;
+  private logger: debug.Debugger;
+
+  constructor(
+    private loggerName: string,
+    private level: LogLevel = LogLevel.debug,
+  ) {
+    const logger = (this.logger = debug(loggerName));
+
+    this.traceLogger = logger.extend('trace');
+    this.debugLogger = logger.extend('debug');
+    this.infoLogger = logger.extend('info');
+    this.warnLogger = logger.extend('warn');
+    this.errorLogger = logger.extend('error');
+    this.fatalLogger = logger.extend('fatal');
+  }
+
+  public childLogger(childLoggerName: string): DebugLogger {
+    return new DebugLogger(`${this.loggerName}:${childLoggerName}`);
+  }
+
+  public setLogLevel(level: LogLevel): void {
+    this.level = level;
+  }
+
+  public debug(...data: [unknown, ...unknown[]]): void {
+    if (this.level < LogLevel.debug) {
+      this.debugLogger(...data);
+    }
+  }
+
+  public error(...data: [unknown, ...unknown[]]): void {
+    if (this.level < LogLevel.error) {
+      this.errorLogger(...data);
+    }
+  }
+
+  public fatal(...data: [unknown, ...unknown[]]): void {
+    if (this.level < LogLevel.fatal) {
+      this.fatalLogger(...data);
+    }
+  }
+
+  public info(...data: [unknown, ...unknown[]]): void {
+    if (this.level < LogLevel.info) {
+      this.infoLogger(...data);
+    }
+  }
+
+  public trace(...data: [unknown, ...unknown[]]): void {
+    if (this.level < LogLevel.trace) {
+      this.traceLogger(...data);
+    }
+  }
+
+  public warn(...data: [unknown, ...unknown[]]): void {
+    if (this.level < LogLevel.warn) {
+      this.warnLogger(...data);
+    }
+  }
 }
 
 export class ConsoleLogger implements Logger {
@@ -85,4 +155,4 @@ export class ConsoleLogger implements Logger {
   }
 }
 
-export const logger = new ConsoleLogger('cogment');
+export const logger = new DebugLogger('cogment');
