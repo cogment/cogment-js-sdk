854a240034333e6f2352283e9ed33cbfb24db3f7
Emma Coutu <emma@coutu.io>
2021-02-03 13:47:09 -0500

    refactor(tsconfig): refactor generated typings to fix namespacing

--- __tests__/end-to-end/cogment-app/webapp/src/cog_settings.js
+++ __tests__/end-to-end/cogment-app/webapp/src/cog_settings.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  *  Copyright 2021 Artificial Intelligence Redefined <dev+cogment@ai-r.com>
  *
@@ -14,50 +15,62 @@
  *  limitations under the License.
  *
  */
-
-const data_pb = require('./data_pb');
-
-const _client_class = {
-  id: 'client',
-  config_type: null,
-  action_space: data_pb.ClientAction,
-  observation_space: data_pb.Observation,
-  observation_delta: data_pb.Observation,
-  observation_delta_apply_fn: (x) => x,
-  feedback_space: null,
-  message_space: null,
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
-
-const _echo_class = {
-  id: 'echo',
-  config_type: null,
-  action_space: data_pb.EchoAction,
-  observation_space: data_pb.Observation,
-  observation_delta: data_pb.Observation,
-  observation_delta_apply_fn: (x) => x,
-  feedback_space: null,
-  message_space: null,
-};
-
-const settings = {
-  actor_classes: {
-    client: _client_class,
-    echo: _echo_class,
-  },
-
-  trial: {
-    config_type: data_pb.TrialConfig,
-  },
-
-  environment: {
-    config_type: data_pb.EnvConfig,
-  },
-
-  env_class: {
-    id: 'env',
+Object.defineProperty(exports, "__esModule", { value: true });
+var data_pb = __importStar(require("./data_pb.js"));
+var _client_class = {
+    id: 'client',
     config_type: null,
-    message_space: null,
-  },
+    action_space: data_pb.ClientAction,
+    observation_space: data_pb.Observation,
+    observation_delta: data_pb.Observation,
+    observation_delta_apply_fn: function (x) { return x; },
+    feedback_space: null,
+    message_space: null
 };
-
-module.exports = settings;
+var _echo_class = {
+    id: 'echo',
+    config_type: null,
+    action_space: data_pb.EchoAction,
+    observation_space: data_pb.Observation,
+    observation_delta: data_pb.Observation,
+    observation_delta_apply_fn: function (x) { return x; },
+    feedback_space: null,
+    message_space: null
+};
+var settings = {
+    actor_classes: {
+        client: _client_class,
+        echo: _echo_class,
+    },
+    trial: {
+        config_type: data_pb.TrialConfig,
+    },
+    environment: {
+        config_type: data_pb.EnvConfig,
+    },
+    env_class: {
+        id: 'env',
+        config_type: null,
+        message_space: null
+    }
+};
+exports.default = settings;
